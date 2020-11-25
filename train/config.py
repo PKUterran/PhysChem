06a683e3c5485ee1ce3397833d9e7f4f21e007df
PKUterran <374308602@qq.com>
2020-11-25 20:53:20 +0800

    test train_qm9 on server

--- train/config.py
+++ train/config.py
@@ -6,22 +6,22 @@ DEFAULT_CONFIG = {
     'MV_DIM': 64,
     'ME_DIM': 64,
     'MM_DIM': 64,
-    'PQ_DIM': 16,
+    'PQ_DIM': 8,
     'N_LAYER': 4,
     'N_HOP': 2,
     'N_ITERATION': 10,
     'N_GLOBAL': 2,
-    'MESSAGE_TYPE': 'norm_naive',
+    'MESSAGE_TYPE': 'triplet',
     'UNION_TYPE': 'gru',
     'DERIVATION_TYPE': 'newton',
     'TAU': 0.02,
-    'DROPOUT': 0.2,
+    'DROPOUT': 0.0,
 
     'EPOCH': 50,
-    'BATCH': 32,
+    'BATCH': 20,
     'LAMBDA': 1,
     'LR': 1e-4,
-    'DECAY': 1e-5,
+    'DECAY': 2e-5,
 }
 
 QM9_CONFIG = DEFAULT_CONFIG.copy()
