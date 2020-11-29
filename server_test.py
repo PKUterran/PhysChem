163ba700d14bdd5b2d369bcaf59b8fcd9ea839da
PKUterran <374308602@qq.com>
2020-11-29 16:36:20 +0800

    lambda 1e-2 vs 1

--- server_test.py
+++ server_test.py
@@ -3,6 +3,32 @@ from train.train_qm9 import train_qm9
 
 # cache_qm9()
 train_qm9(max_num=5000, data_name='QM9-5000', tag='QM9-5000',
+          use_tqdm=False, force_save=True, use_cuda=True,
+          special_config={
+              'HV_DIM': 16,
+              'HE_DIM': 16,
+              'HM_DIM': 16,
+              'MV_DIM': 16,
+              'ME_DIM': 16,
+              'MM_DIM': 16,
+              'PQ_DIM': 16,
+              'N_LAYER': 4,
+              'N_HOP': 3,
+              'N_ITERATION': 10,
+              'N_GLOBAL': 3,
+              'MESSAGE_TYPE': 'triplet',
+              'UNION_TYPE': 'gru',
+              'DERIVATION_TYPE': 'newton',
+              'TAU': 0.02,
+              'DROPOUT': 0.0,
+
+              'EPOCH': 100,
+              'BATCH': 32,
+              'LAMBDA': 1,
+              'LR': 4e-4,
+              'DECAY': 1e-5,
+          })
+train_qm9(max_num=5000, data_name='QM9-5000', tag='QM9-5000-lambda1e-2',
           use_tqdm=False, force_save=False, use_cuda=True,
           special_config={
               'HV_DIM': 16,
@@ -23,8 +49,8 @@ train_qm9(max_num=5000, data_name='QM9-5000', tag='QM9-5000',
               'DROPOUT': 0.0,
 
               'EPOCH': 100,
-              'BATCH': 16,
-              'LAMBDA': 1,
+              'BATCH': 32,
+              'LAMBDA': 1e-2,
               'LR': 4e-4,
               'DECAY': 1e-5,
           })
