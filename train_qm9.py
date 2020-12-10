c3f8a197974e225460901ec0cc02ca6a7ef351a8
YangShuwen <374308602@qq.com>
2020-12-10 11:03:04 +0800

    train qm9

--- train_qm9.py
+++ train_qm9.py
@@ -3,17 +3,6 @@ from train.train_qm9 import train_qm9
 
 # cache_qm9()
 train_qm9(
-    special_config={},
-    use_cuda=True,
-    max_num=-1,
-    data_name='QM9',
-    seed=0,
-    force_save=True,
-    tag='QM9',
-    use_tqdm=False,
-)
-
-train_qm9(
     special_config={
         'Xconf': True,
     },
@@ -23,5 +12,16 @@ train_qm9(
     seed=0,
     force_save=False,
     tag='QM9-Xconf',
+    use_tqdm=False,
+)
+
+train_qm9(
+    special_config={},
+    use_cuda=True,
+    max_num=-1,
+    data_name='QM9',
+    seed=0,
+    force_save=True,
+    tag='QM9',
     use_tqdm=False,
 )
