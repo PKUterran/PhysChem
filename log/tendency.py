26e0f1d029280f17bb9cb034537b02104716ee2a
PKUterran <374308602@qq.com>
2020-12-11 12:53:05 +0800

    server

--- log/tendency.py
+++ log/tendency.py
@@ -12,11 +12,12 @@ def tendency_pc(log: dict, path: str):
     fig, ax1 = plt.subplots()
     ax1.plot(epochs, train_p, color='red', linestyle='--')
     ax1.plot(epochs, test_p, color='red')
-    ax1.set_ylim(2, 11)
-    ax2 = ax1.twinx()
-    ax2.plot(epochs, train_c, color='green', linestyle='--')
-    ax2.plot(epochs, test_c, color='green')
-    ax2.set_ylim(0.7, 1.8)
+    ax1.set_ylim(min(train_p) - 0.5, max(train_p) + 0.5)
+    if max(train_c) > min(train_c) + 1e-4:
+        ax2 = ax1.twinx()
+        ax2.plot(epochs, train_c, color='green', linestyle='--')
+        ax2.plot(epochs, test_c, color='green')
+        ax2.set_ylim(min(train_c) - 0.1, max(train_c) + 0.1)
     plt.savefig(path)
     plt.close(fig)
 
--- train_qm9.py
+++ train_qm9.py
@@ -2,18 +2,18 @@ from data.qm9.load_qm9 import cache_qm9
 from train.train_qm9 import train_qm9
 
 # cache_qm9()
-train_qm9(
-    special_config={
-        'Xconf': True,
-    },
-    use_cuda=True,
-    max_num=-1,
-    data_name='QM9',
-    seed=0,
-    force_save=True,
-    tag='QM9-Xconf',
-    use_tqdm=False,
-)
+# train_qm9(
+#     special_config={
+#         'Xconf': True,
+#     },
+#     use_cuda=True,
+#     max_num=-1,
+#     data_name='QM9',
+#     seed=0,
+#     force_save=True,
+#     tag='QM9-Xconf',
+#     use_tqdm=False,
+# )
 
 train_qm9(
     special_config={},
@@ -22,6 +22,6 @@ train_qm9(
     data_name='QM9',
     seed=0,
     force_save=False,
-    tag='QM9',
+    tag='QM9-',
     use_tqdm=False,
 )
