0700dfe0d69abb670245143126e9fab5c59df810
YangShuwen <374308602@qq.com>
2020-12-12 23:14:32 +0800

    LR/DECAY

--- log/tendency.py
+++ log/tendency.py
@@ -24,6 +24,7 @@ def tendency_pc(log: dict, path: str):
 
 tuples = [
     ('QM9', 'QM9-Xconf'),
+    ('QM9', 'QM9-'),
     # ('QM9', 'QM9-10000-Xconf-naive'),
     # ('QM9', 'QM9-5000-naive'),
     # ('QM9', 'QM9-5000'),
--- train_qm9.py
+++ train_qm9.py
@@ -14,18 +14,18 @@ from train.train_qm9 import train_qm9
 #     tag='QM9-Xconf',
 #     use_tqdm=False,
 # )
-train_qm9(
-    special_config={
-
-    },
-    use_cuda=True,
-    max_num=-1,
-    data_name='QM9',
-    seed=0,
-    force_save=False,
-    tag='QM9-',
-    use_tqdm=False,
-)
+# train_qm9(
+#     special_config={
+#
+#     },
+#     use_cuda=True,
+#     max_num=-1,
+#     data_name='QM9',
+#     seed=0,
+#     force_save=False,
+#     tag='QM9-',
+#     use_tqdm=False,
+# )
 
 train_qm9(
     special_config={
