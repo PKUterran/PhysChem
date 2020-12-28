4cc041968fdc0f4181e18857602a5d2e067aeaf0
YangShuwen <374308602@qq.com>
2020-12-28 16:25:12 +0800

    FreeSolv

--- freesolv.py
+++ freesolv.py
@@ -1,26 +1,25 @@
 from net.config import ConfType
 from train.train_single_regression import train_single_regression, SingleRegressionDataset
 
-train_single_regression(
-    dataset=SingleRegressionDataset.FREESOLV,
-    data_name='FreeSolv',
-    tag='FreeSolv',
-    special_config={
-
-    },
-    use_cuda=True,
-    max_num=-1,
-    seed=0,
-    force_save=False,
-    use_tqdm=False
-)
+# train_single_regression(
+#     dataset=SingleRegressionDataset.FREESOLV,
+#     data_name='FreeSolv',
+#     tag='FreeSolv',
+#     special_config={
+#
+#     },
+#     use_cuda=True,
+#     max_num=-1,
+#     seed=0,
+#     force_save=False,
+#     use_tqdm=False
+# )
 train_single_regression(
     dataset=SingleRegressionDataset.FREESOLV,
     data_name='FreeSolv-Xconf',
     tag='FreeSolv-Xconf',
     special_config={
         'CONF_TYPE': ConfType.NONE,
-        'MESSAGE_TYPE': 'naive',
     },
     use_cuda=True,
     max_num=-1,
