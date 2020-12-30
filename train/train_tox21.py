4621b67ca6fb0df1aa636543f3fc2351fa343b24
PKUterran <374308602@qq.com>
2020-12-30 19:38:43 +0800

    tox21.py

--- train/train_tox21.py
+++ train/train_tox21.py
@@ -99,7 +99,7 @@ def train_tox21(special_config: dict = None,
 
     def nan_masked(s: torch.Tensor, t: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         nan_mask = torch.isnan(t).type(torch.float32)
-        not_nan_mask = torch.logical_not(torch.isnan(t)).type(torch.float32)
+        not_nan_mask = -nan_mask + 1.0
         s = s * not_nan_mask + nan_mask * 1e-6
         t = t * not_nan_mask
         t[torch.isnan(t)] = 0
