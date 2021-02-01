6138c075458b956a1d4fa3da2271cd72931d7cae
YangShuwen <374308602@qq.com>
2021-02-01 11:38:02 +0800

    qm9-lambda10

--- qm9.py
+++ qm9.py
@@ -3,13 +3,13 @@ from train.train_qm9 import train_qm9
 
 train_qm9(
     special_config={
-
+        'LAMBDA': 10
     },
     use_cuda=True,
     max_num=-1,
     data_name='QM9',
     seed=0,
     force_save=False,
-    tag='QM9',
+    tag='QM9-lambda10',
     use_tqdm=False,
 )
