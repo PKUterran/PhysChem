cf4f0de5cf4f096a6a064e449869dec2ac7d409b
PKUterran <374308602@qq.com>
2021-08-05 23:25:10 +0800

    kabsch-rmsd

--- RMSD.py
+++ RMSD.py
@@ -8,6 +8,7 @@ from visualize.eval_rmsd import eval_rmsd_with_mols
 
 '''
 scp 1500011335@115.27.161.31:yangshuwen/GeomNN/train/models/QM9-Oconf* train/models/
+scp 1500011335@115.27.161.31:yangshuwen/GeomNN/log/QM9/QM9-Oconf-Kabsch.json log/QM9
 Available: 19633/20000
 Guess: 1.852809122054676
 RDKit: 0.7347384707025154
@@ -15,6 +16,14 @@ CVGAE: 1.2589760663587264
 HamEng: 0.7194251172988463
 GeomNN: 0.5809578502960406
 GeomNN better than RDKit: 12204/19633
+
+Available: 128537/133247
+Guess: 1.9723017399877953
+RDKit: 0.8047841509522299
+CVGAE: 1.3638266570626785
+HamEng: 0.8262426909693438
+GeomNN: 0.7087681227842518
+GeomNN better than RDKit: 72470/128537
 '''
 
 
